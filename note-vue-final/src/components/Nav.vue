71963208d75a6294a494d4985a30314a9a22d977
Sargazi77 <sargazi1377@gmail.com>
2020-05-14 10:06:51 -0500

    updated

--- note-vue-final/src/components/Nav.vue
+++ note-vue-final/src/components/Nav.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="navbar">
     <b-nav >
-    <b-nav-item href="#1">Logout</b-nav-item>
-    <b-nav-item href="#2">Github</b-nav-item>
+    <b-nav-item href="/..." v-b-tooltip.hover title="Comming Soon...">Logout</b-nav-item>
+    <b-nav-item href="https://github.com/Sargazi77/2_Final"v-b-tooltip.hover title="Go to Github ">Github</b-nav-item>
     <b-nav-text id="text">Welcome User (admin)</b-nav-text>
   </b-nav>
 
